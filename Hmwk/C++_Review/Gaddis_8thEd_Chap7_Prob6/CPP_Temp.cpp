3eade759242e1792af5d2cde4815464825664fa5
jmora526 <jmorales4255@gmail.com>
2016-09-13 12:13:16 -0700

    Signed-off-by: jmora526 <jmorales4255@gmail.com>

---
+++ Hmwk/C++_Review/Gaddis_8thEd_Chap7_Prob6/CPP_Temp.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File: Rain or Shine
+ * Author: Jose Morales
+ * Purpose: Temp for all the programs
+ * Created on September 12, 2016, 12:12 PM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process Data
+    
+    //Output Data
+    
+    return 0;
+}
+
