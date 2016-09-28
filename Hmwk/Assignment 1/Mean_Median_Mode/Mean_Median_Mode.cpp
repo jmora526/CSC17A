b7ec3bae6354f036d9c070ad43226a35195c0766
jmora526 <jmorales4255@gmail.com>
2016-09-28 01:57:10 -0700

    Signed-off-by: jmora526 <jmorales4255@gmail.com>

---
+++ Hmwk/Assignment 1/Mean_Median_Mode/Mean_Median_Mode.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File: Mean,Median,Mode
+ * Author: Jose Morales
+ * Purpose: Make a mean,median,and mode
+ * Created on September 26, 2016, 1:56 PM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+   
+    return 0;
+}
+
